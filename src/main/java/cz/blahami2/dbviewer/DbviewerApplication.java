51c41b5419b64d7ef056d719787d46ea60806024
Michael Blaha <michael.blaha@deutsche-boerse.com>
2018-06-21 16:29:20 +0200

    Initial commit with Spring initializer

---
+++ src/main/java/cz/blahami2/dbviewer/DbviewerApplication.java
@@ -0,0 +1,12 @@
+package cz.blahami2.dbviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbviewerApplication.class, args);
+	}
+}
---
+++ src/test/java/cz/blahami2/dbviewer/DbviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.blahami2.dbviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbviewerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
