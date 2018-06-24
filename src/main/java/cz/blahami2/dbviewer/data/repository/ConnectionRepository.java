f6b702ea81c1d9d7ed401f77f7d6eb73bc57b7f9
Michael Blaha <blahami2@gmail.com>
2018-06-24 11:24:24 +0200

    #2: added Connections repository

---
+++ src/main/java/cz/blahami2/dbviewer/data/repository/ConnectionRepository.java
@@ -0,0 +1,8 @@
+package cz.blahami2.dbviewer.data.repository;
+
+import cz.blahami2.dbviewer.data.entity.Connection;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConnectionRepository extends CrudRepository<Connection, Long> {
+
+}
